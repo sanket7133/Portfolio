b25f4c3894bb4f495a9c8a2543177d7ab7a6ef25
Sanket <mrsanket45@gmail.com>
2024-07-29 00:00:20 +0530

    "Updated portfolio website: reformatted PDF, added new project cards, and implemented Owl Carousel for project section; also made minor styling changes and added JavaScript code for carousel functionality."

--- script.js
+++ script.js
@@ -4,3 +4,38 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+// const projectCardsContainer = document.querySelector('.project-cards-container');
+// const nextButton = document.getElementById('next-button');
+// let currentCardIndex = 0;
+
+// nextButton.addEventListener('click', () => {
+//   const cards = projectCardsContainer.children;
+//   const cardWidth = cards[0].offsetWidth + 20; // 20 is the margin right
+//   const maxCardsToShow = 3;
+//   const totalCards = cards.length;
+
+//   if (currentCardIndex + maxCardsToShow < totalCards) {
+//     currentCardIndex++;
+//     projectCardsContainer.style.transform = `translateX(-${currentCardIndex * cardWidth}px)`;
+//   }
+// });
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    loop: false,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      }
+    }
+  });
+});
